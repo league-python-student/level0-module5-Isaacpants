928bc9e6faa3cd0ade0808c52fb1631a25a17de8
root <root@root.com>
2021-08-01 19:50:06 +0000

    finished level 0

--- _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
+++ _03_nested_loops/for_loop_gauntlet/for_loop_gauntlet.py
@@ -3,22 +3,42 @@ Create algorithms and use to solve
 https://central.jointheleague.org/levels/Level0/Recipes/ForLoopGauntlet.html
 """
 
-
 if __name__ == '__main__':
-    for i in range(100):
-        print(i+1)
-    for j in range(100):
-        print(100-j)
-    for k in range(501):
-        if k%2 == 0:
-            print(str(k) + ', even')
-        else:
-            print(str(k) + ', odd')
-    for l in range(778):
-        if l % 7 ==0:
-            print(str(l))
+    # for i in range(100):
+    #     print(i+1)
+    #
+    # for j in range(100):
+    #     print(100-j)
+    #
+    # for k in range(501):
+    #     if k%2 == 0:
+    #         print(str(k) + ', even')
+    #     else:
+    #         print(str(k) + ', odd')
+    #
+    # for l in range(778):
+    #     if l % 7 ==0:
+    #         print(str(l))
+
     # for m in range(15):
-    #     print("As of " + 2021 - m+ ", I am " + 15 - m)
+    #      print("As of " + str(2021 - m) + ", I am " + str(15 - m))
+    #
+    # for n in range(3):
+    #     for o in range(3):
+    #         print(str(n) + " " + str(o) + " ")
 
+    # for p in range(3):
+    #     print("\n" + str(p + p + p + 1) + str(p + p + p + 2) + str(p + p + p + 3))
+    #
+    # for q in range(1, 101):
+    #     print(str(q), end=" ")
+    #     if q % 10 == 0:
+    #         print("\n")
 
-    #pass
+    for r in range(2, 8):
+        for s in range(r):
+            print("*", end=" ")
+            if s % r == 0:
+                print("\n")
+
+# pass
